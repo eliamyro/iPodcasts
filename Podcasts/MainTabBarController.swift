a9edb8d8722b27c9c11c506a59c048d87d6abe78
eliamyro <elias.myronidis@hotmail.com>
2019-08-12 01:02:27 +0300

    Maximizing and Minimizing Player Animations

--- Podcasts/MainTabBarController.swift
+++ Podcasts/MainTabBarController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class MainTabController: UITabBarController {
     
+    var maximizedTopAnchorConstraint: NSLayoutConstraint!
+    var minimizedTopAnchorConstraint: NSLayoutConstraint!
     
     // MARK: - UIViewController Methods
     
@@ -19,15 +21,20 @@ class MainTabController: UITabBarController {
         view.backgroundColor = .yellow
         tabBar.tintColor = .purple
         
+        setupViewControllers()
+        
+        setupPlayerDetailsView()
+    }
+    
+    // MARK: - MainTabController Methods
+    
+    private func setupViewControllers() {
         viewControllers = [
             generateNavigationControllers(with: PodcastsSearchController(), title: "Search", image: #imageLiteral(resourceName: "search")),
             generateNavigationControllers(with: ViewController(), title: "Favorites", image: #imageLiteral(resourceName: "favorites")),
             generateNavigationControllers(with: ViewController(), title: "Downloads", image: #imageLiteral(resourceName: "downloads"))
         ]
     }
-    
-    
-    // MARK: - MainTabController Methods
     
     private func generateNavigationControllers(with rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
         let navController = UINavigationController(rootViewController: rootViewController)
@@ -37,5 +44,44 @@ class MainTabController: UITabBarController {
         navController.tabBarItem.image = image
         
         return navController
+    }
+    
+    private func setupPlayerDetailsView() {
+        let playerDetailsView = PlayerDetailsView()
+        playerDetailsView.backgroundColor = .red
+        playerDetailsView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.insertSubview(playerDetailsView, belowSubview: tabBar)
+        
+        maximizedTopAnchorConstraint = playerDetailsView.topAnchor.constraint(equalTo: view.topAnchor, constant: view.frame.height)
+        maximizedTopAnchorConstraint.isActive = true
+        
+        minimizedTopAnchorConstraint = playerDetailsView.topAnchor.constraint(equalTo: tabBar.topAnchor, constant: -64)
+//        minimizedTopAnchorConstraint.isActive = true
+        
+        playerDetailsView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        playerDetailsView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        playerDetailsView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        perform(#selector(maximizePlayerDetailsView), with: nil, afterDelay: 1)
+    }
+    
+    @objc private func minimizePlayerDetailsView() {
+        maximizedTopAnchorConstraint.isActive = false
+        minimizedTopAnchorConstraint.isActive = true
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    @objc private func  maximizePlayerDetailsView() {
+        minimizedTopAnchorConstraint.isActive = false
+        maximizedTopAnchorConstraint.isActive = true
+        maximizedTopAnchorConstraint.constant = 0
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.view.layoutIfNeeded()
+        })
     }
 }
